29a2f2f41d59e4a13f7f229a2a485dc4f59542af
jvecodev <jvitor.oliveira1803@gmail.com>
2024-08-14 11:55:05 -0300

    Condicionais e Switch Cas

---
+++ src/study/jvecodev/maratonajava/primeirospassos/condicionais/Estruturas03.java
@@ -0,0 +1,27 @@
+package study.jvecodev.maratonajava.primeirospassos.condicionais;
+
+/**
+  Operacao Ternária
+ */
+public class Estruturas03 {
+    public static void main(String[] args) {
+        //utilizada para facilitar condicionais
+        //Sintaxe -> variavel = (condicao) ? True : False;
+
+        double salario = 3600;
+        double precops5 = 3000;
+
+        String comprar = "Devo comprar o videogame";
+        String naoComprar = "Não devo comprar o videogame";
+        String resultado = salario >= precops5 ? comprar : naoComprar;
+
+        System.out.println(resultado);
+
+        //Outra maneira
+
+//        System.out.println(resultado = salario >= precops5 ? comprar : naoComprar);
+
+
+
+    }
+}
---
+++ src/study/jvecodev/maratonajava/primeirospassos/condicionais/PrimeiroExercicio.java
@@ -0,0 +1,33 @@
+package study.jvecodev.maratonajava.primeirospassos.condicionais;
+
+
+import java.util.Scanner;
+
+public class PrimeiroExercicio {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double imposto;
+        System.out.print("Digite seu salario: ");
+        double valor = Integer.parseInt(scanner.nextLine());
+
+        if (valor > 0 && valor <= 3472){
+
+            imposto = (9.70/100) * valor;
+
+        }
+        else if (valor > 3472 && valor <= 68507 ) {
+
+            imposto = (37.35 / 100) * valor;
+
+        }
+        else {
+
+            imposto = (49.50 / 100) * valor;
+
+        }
+
+        System.out.printf("Definido o valor do seu imposto anual → %.2f", imposto);
+
+
+    }
+}
